13a6fb5e3f64891356d934361685949691c544d3
gabrielLCSC <gabriel.dautreppe@gmail.com>
2024-10-22 14:55:55 +0200

    feat: hero section w/out responsive

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,9 +1,36 @@
 import './index.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Hero() {
     return (
-        <div>
-            Hero
+        <div className='hero'>
+            <div className='hero__content'>
+                <h1 className='hero__content__title'>super title that’s very <strong>very cool</strong> and useful</h1>
+                <div className='hero__content__card'>
+                    <h2 className='hero__content__card__title'>Fluent with Ambre</h2>
+                    <p className='hero__content__card__text'>Lorem ipsum dolor sic amed amed amed ametLorem ipsum dolor sic amed amed amed amet</p>
+                </div>
+                <Link
+                    className='hero__content__card__link'
+                    href="#"
+                >Voir les disponibilités
+                <Image 
+                src="/images/calendar_white.svg"
+                width={24}
+                height={24}
+                alt='calendar_white'
+                />
+                </Link>
+
+            </div>
+            <Image
+            className='hero__illu'
+                    src="/images/illu_hero.png"
+                    width={521}
+                    height={648}
+                    alt='illu_hero'
+                    />
         </div>
     )
 }
